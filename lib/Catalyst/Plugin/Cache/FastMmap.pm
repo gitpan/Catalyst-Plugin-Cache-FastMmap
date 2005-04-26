134c595e06126ca7a4ed31f91945bb5325043850
Sebastian Riedel <kraihx@googlemail.com>
2005-04-26 23:39:50 +0000

    Import of SRI/Catalyst-Plugin-Cache-FastMmap-0.5 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-Cache-FastMmap
    gitpan-cpan-version:      0.5
    gitpan-cpan-path:         SRI/Catalyst-Plugin-Cache-FastMmap-0.5.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

---
+++ lib/Catalyst/Plugin/Cache/FastMmap.pm
@@ -0,0 +1,88 @@
+package Catalyst::Plugin::Cache::FastMmap;
+
+use strict;
+use base 'Class::Data::Inheritable';
+
+use Cache::FastMmap;
+
+our $VERSION= '0.5';
+
+__PACKAGE__->mk_classdata('cache');
+
+sub setup {
+    my $self = shift;
+
+    my %params = ();
+
+    if ( $self->config->{cache} ) {
+        %params = %{ $self->config->{cache} };
+    }
+
+    if ( $params{storage} ) {
+        $params{share_file} = delete $params{storage};
+    }
+
+    if ( $params{expires} ) {
+        $params{expire_time} = delete $params{expires};
+    }
+
+    $self->cache( Cache::FastMmap->new(%params) );
+
+    return $self->NEXT::setup(@_);
+}
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+Catalyst::Plugin::Cache::FastMmap - Mmap cache
+
+=head1 SYNOPSIS
+
+    use Catalyst qw[Cache::FastMmap];
+
+    MyApp->config->{cache}->{storage} = '/tmp';
+    MyApp->config->{cache}->{expires} = 3600;
+
+    my $data;
+
+    unless ( $data = $c->cache->get('data') ) {
+        $data = MyApp::Model::Data->retrieve('data');
+        $c->cache->set( 'data', $data );
+    }
+
+    $c->response->body($data);
+
+
+=head1 DESCRIPTION
+
+Extends base class with a C<mmap> cache.
+
+=head1 METHODS
+
+=over 4
+
+=item cache
+
+Returns an instance of C<Cache::FastMmap>
+
+=back
+
+=head1 SEE ALSO
+
+L<Cache::FastMmap>, L<Catalyst>.
+
+=head1 AUTHOR
+
+Christian Hansen, C<ch@ngmedia.com>
+Sebastian Riedel C<sri@oook.de>
+
+=head1 LICENSE
+
+This library is free software . You can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
